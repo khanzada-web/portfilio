310a42732ee1e9eea353b631a3ad9f41855b3da3
khanzada-web <zada38843@gmail.com>
2026-08-04 01:55:12 +0500

    home page added

--- app/layout.tsx
+++ app/layout.tsx
@@ -250,13 +250,6 @@ export default function RootLayout({
     <html lang="en" dir="ltr">
       <head>
         <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9383132900869188"
-          crossOrigin="anonymous"
-          suppressHydrationWarning
-        />
-
-        <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P3GKJN4G');`,
