abb8a60b040c9323828fdf6bc7a38ca7a18ac869
khanzada-web <zada38843@gmail.com>
2026-08-04 01:54:08 +0500

    Update GTM container ID to GTM-P3GKJN4G

--- app/layout.tsx
+++ app/layout.tsx
@@ -259,7 +259,7 @@ export default function RootLayout({
         <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KK3WH3FD');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P3GKJN4G');`,
           }}
         />
 
@@ -281,7 +281,7 @@ export default function RootLayout({
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KK3WH3FD"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3GKJN4G"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
